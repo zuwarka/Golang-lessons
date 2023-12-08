8d314b2c6279aa78a142200c4c10f3756f2949a3
zuwarka <soap.sony@gmail.com>
2023-12-08 18:56:58 +0300

    Игра Жизнь (Cornways Game of Life)

--- conways_game/main.go
+++ conways_game/main.go
@@ -10,11 +10,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 const (
-	height = 10
-	width  = 10
+	height    = 10
+	width     = 10
+	countShow = 300
 )
 
 type Universe [][]bool
@@ -49,8 +51,62 @@ func (u Universe) Seed() {
 	}
 }
 
+func (u Universe) Alive(x, y int) bool {
+	x = (x + width) % width
+	y = (y + height) % height
+	return u[y][x]
+}
+
+func (u Universe) Neighbors(x, y int) int {
+	n := 0
+	for v := -1; v <= 1; v++ {
+		for h := -1; h <= 1; h++ {
+			if !(v == 0 && h == 0) && u.Alive(x+h, y+v) {
+				n++
+			}
+		}
+	}
+	return n
+}
+
+func (u Universe) Next(x, y int) bool {
+	n := u.Neighbors(x, y)
+	return n == 3 || n == 2 && u.Alive(x, y)
+}
+
+func Step(a, b Universe) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			b[y][x] = a.Next(x, y)
+		}
+	}
+}
+
+// CheckAliveUniverse функция, которая проверяет состояние Вселенной на все false
+func (u Universe) CheckAliveUniverse() bool {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if u[y][x] == true {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func main() {
-	a := NewUniverse()
+	a, b := NewUniverse(), NewUniverse()
 	a.Seed()
-	a.Show()
+
+	for i := 0; i < countShow; i++ {
+		Step(a, b)
+		if !a.CheckAliveUniverse() { // если все false - останавливаем цикл
+			break
+		}
+		a.Show()
+		fmt.Print("\x0c")
+		time.Sleep(time.Second)
+		a, b = b, a
+	}
 }
